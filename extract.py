f939b60d3408bf137cf3144a90163a1e38fb3015
Luis <luis.abello@usach.cl>
2022-01-26 01:13:46 -0300

    asdsadsa

--- extract.py
+++ extract.py
@@ -1,6 +1,6 @@
 import zipfile, os
 
-working_directory = './experimentos/imagenes/'
+working_directory = './home/DIINF/U-net-original/experimentos/imagenes/'
 os.chdir(working_directory)
 
 for file in os.listdir(working_directory):   # get the list of files
