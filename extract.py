fa3a5b3c9160b642d0f3e4486f3700132d18c12e
Luis <luis.abello@usach.cl>
2022-01-26 01:19:40 -0300

    dasdasd

--- extract.py
+++ extract.py
@@ -4,6 +4,7 @@ working_directory = '/home/DIINF/labello/U-net-original/experimentos/imagenes/'
 os.chdir(working_directory)
 
 for file in os.listdir(working_directory):   # get the list of files
+    print(file)
     if zipfile.is_zipfile(file): # if it is a zipfile, extract it
         with zipfile.ZipFile(file) as item: # treat the file as a zip
            item.extractall()  # extract it in the working directory
