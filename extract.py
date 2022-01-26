2ce34c6c8e54c41b5d76740c1de4dd439ebd17e8
Luis <luis.abello@usach.cl>
2022-01-26 01:06:01 -0300

    add: extraer zip

---
+++ extract.py
@@ -0,0 +1,9 @@
+import zipfile, os
+
+working_directory = '/experimentos/imagenes/'
+os.chdir(working_directory)
+
+for file in os.listdir(working_directory):   # get the list of files
+    if zipfile.is_zipfile(file): # if it is a zipfile, extract it
+        with zipfile.ZipFile(file) as item: # treat the file as a zip
+           item.extractall()  # extract it in the working directory
