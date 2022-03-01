80c2a97e2fd4e3f99351c794618a622e66dd58c9
Luis <luis.abello@usach.cl>
2022-03-01 01:35:06 -0300

    dsdsad

--- train3.py
+++ train3.py
@@ -693,7 +693,7 @@ if __name__ == "__main__":
     lr = 1e-4   ## 0.0001
     num_epochs = 150
 
-    for i in range(7, 11):
+    for i in range(5, 11):
         model_path = "files-con-dos-perdidas/model-aumento-con-dos-perdidas"+str(i)+".h5"
         csv_path = "files-con-dos-perdidas/data-aumento-con-dos-perdidas"+str(i)+".csv"
 
