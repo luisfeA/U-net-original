f24cf11b78e3d501290968303ae4e8b946e8bf96
Luis <luis.abello@usach.cl>
2022-03-13 01:22:54 -0300

    asasA

--- eval.py
+++ eval.py
@@ -10,6 +10,15 @@ from tensorflow.keras.utils import CustomObjectScope
 from sklearn.metrics import accuracy_score, f1_score, jaccard_score, precision_score, recall_score
 from metrics import dice_coef, iou
 from train import load_data
+from train import load_data2
+from train import load_data3
+from train import load_data4
+from train import load_data5
+from train import load_data6
+from train import load_data7
+from train import load_data8
+from train import load_data9
+from train import load_data10
 
 H = 580
 W = 780
@@ -57,46 +66,134 @@ if __name__ == "__main__":
 
     """ Dataset """
     dataset_path = "experimentos/imagenes/"
-    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(dataset_path)
-    """ Prediction and metrics values """
-    SCORE = []
-    for x, y in tqdm(zip(test_x, test_y), total=len(test_x)):
-        name = x.split("/")[-1]
-        print(name)
-        """ Reading the image and mask """
-        ori_x, x = read_image(x)
-        ori_y, y = read_mask(y)
-        """ Prediction """
-        y_pred = model.predict(x)[0] > 0.5
-        #print(y_pred)
-        y_pred = np.squeeze(y_pred, axis=-1)
-        #print(y_pred)
-        y_pred = y_pred.astype(np.int32)
-        #print(y_pred)
-        save_path = f"/home/DIINF/labello/U-net-original/experimentos/resultados/{name}"
-        save_result(ori_x, ori_y, y_pred, save_path)
+    create_dir("experimentos/resultados/experimento1")
+    create_dir("experimentos/resultados/experimento2")
+    create_dir("experimentos/resultados/experimento3")
+    create_dir("experimentos/resultados/experimento4")
+    create_dir("experimentos/resultados/experimento5")
+    create_dir("experimentos/resultados/experimento6")
+    create_dir("experimentos/resultados/experimento7")
+    create_dir("experimentos/resultados/experimento8")
+    create_dir("experimentos/resultados/experimento9")
+    create_dir("experimentos/resultados/experimento10")
 
-        """ Flattening the numpy arrays. """
-        y = y.flatten()
-        y_pred = y_pred.flatten()
+    create_dir("experimentos/resultados/experimento1/caso1")
+    create_dir("experimentos/resultados/experimento2/caso1")
+    create_dir("experimentos/resultados/experimento3/caso1")
+    create_dir("experimentos/resultados/experimento4/caso1")
+    create_dir("experimentos/resultados/experimento5/caso1")
+    create_dir("experimentos/resultados/experimento6/caso1")
+    create_dir("experimentos/resultados/experimento7/caso1")
+    create_dir("experimentos/resultados/experimento8/caso1")
+    create_dir("experimentos/resultados/experimento9/caso1")
+    create_dir("experimentos/resultados/experimento10/caso1")
 
-        """ Calculating metrics values """
-        acc_value = accuracy_score(y, y_pred)
-        f1_value = f1_score(y, y_pred, labels=[0, 1], average="binary")
-        jac_value = jaccard_score(y, y_pred, labels=[0, 1], average="binary")
-        recall_value = recall_score(y, y_pred, labels=[0, 1], average="binary")
-        precision_value = precision_score(y, y_pred, labels=[0, 1], average="binary")
-        SCORE.append([name, acc_value, f1_value, jac_value, recall_value, precision_value])
+    create_dir("experimentos/resultados/experimento1/caso2")
+    create_dir("experimentos/resultados/experimento2/caso2")
+    create_dir("experimentos/resultados/experimento3/caso2")
+    create_dir("experimentos/resultados/experimento4/caso2")
+    create_dir("experimentos/resultados/experimento5/caso2")
+    create_dir("experimentos/resultados/experimento6/caso2")
+    create_dir("experimentos/resultados/experimento7/caso2")
+    create_dir("experimentos/resultados/experimento8/caso2")
+    create_dir("experimentos/resultados/experimento9/caso2")
+    create_dir("experimentos/resultados/experimento10/caso2")
 
-    """ Metrics values """
-    score = [s[1:]for s in SCORE]
-    score = np.mean(score, axis=0)
-    print(f"Accuracy: {score[0]:0.5f}")
-    print(f"F1: {score[1]:0.5f}")
-    print(f"Jaccard: {score[2]:0.5f}")
-    print(f"Recall: {score[3]:0.5f}")
-    print(f"Precision: {score[4]:0.5f}")
+    create_dir("experimentos/resultados/experimento1/caso3")
+    create_dir("experimentos/resultados/experimento2/caso3")
+    create_dir("experimentos/resultados/experimento3/caso3")
+    create_dir("experimentos/resultados/experimento4/caso3")
+    create_dir("experimentos/resultados/experimento5/caso3")
+    create_dir("experimentos/resultados/experimento6/caso3")
+    create_dir("experimentos/resultados/experimento7/caso3")
+    create_dir("experimentos/resultados/experimento8/caso3")
+    create_dir("experimentos/resultados/experimento9/caso3")
+    create_dir("experimentos/resultados/experimento10/caso3")
 
-    """ Saving all the results """
-    df = pd.DataFrame(SCORE, columns=["Image", "Accuracy", "F1", "Jaccard", "Recall", "Precision"])
-    df.to_csv("/home/DIINF/labello/U-net-original/files/score.csv")
+    for i in range(1, 11):
+        (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(dataset_path)
+        """ Prediction and metrics values """
+
+        if i == 1:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(dataset_path)
+            save_path = "experimentos/resultados/experimento1/caso1/"
+            cvc_path = "experimentos/resultados/experimento1/caso1/score.csv"
+        elif i == 2:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data2(dataset_path)
+            save_path = "experimentos/resultados/experimento2/caso1/"
+            cvc_path = "experimentos/resultados/experimento2/caso1/score.csv"
+        elif i == 3:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data3(dataset_path)
+            save_path = "experimentos/resultados/experimento3/caso1/"
+            cvc_path = "experimentos/resultados/experimento3/caso1/score.csv"
+        elif i == 4:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data4(dataset_path)
+            save_path = "experimentos/resultados/experimento4/caso1/"
+            cvc_path = "experimentos/resultados/experimento4/caso1/score.csv"
+        elif i == 5:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data5(dataset_path)
+            save_path = "experimentos/resultados/experimento5/caso1/"
+            cvc_path = "experimentos/resultados/experimento5/caso1/score.csv"
+        elif i == 6:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data6(dataset_path)
+            save_path = "experimentos/resultados/experimento6/caso1/"
+            cvc_path = "experimentos/resultados/experimento6/caso1/score.csv"
+        elif i == 7:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data7(dataset_path)
+            save_path = "experimentos/resultados/experimento7/caso1/"
+            cvc_path = "experimentos/resultados/experimento7/caso1/score.csv"
+        elif i == 8:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data8(dataset_path)
+            save_path = "experimentos/resultados/experimento8/caso1/"
+            cvc_path = "experimentos/resultados/experimento8/caso1/score.csv"
+        elif i == 9:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data9(dataset_path)
+            save_path = "experimentos/resultados/experimento9/caso1/"
+            cvc_path = "experimentos/resultados/experimento9/caso1/score.csv"
+        elif i == 10:
+            (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data10(dataset_path)
+            save_path = "experimentos/resultados/experimento10/caso1/"
+            cvc_path = "experimentos/resultados/experimento10/caso1/score.csv"
+
+        SCORE = []
+        for x, y in tqdm(zip(test_x, test_y), total=len(test_x)):
+            name = x.split("/")[-1]
+            print(name)
+            """ Reading the image and mask """
+            ori_x, x = read_image(x)
+            ori_y, y = read_mask(y)
+            """ Prediction """
+            y_pred = model.predict(x)[0] > 0.5
+            #print(y_pred)
+            y_pred = np.squeeze(y_pred, axis=-1)
+            #print(y_pred)
+            y_pred = y_pred.astype(np.int32)
+            #print(y_pred)
+            save_path = save_path+f"{name}"
+            #save_path = f"/home/DIINF/labello/U-net-original/experimentos/resultados/{name}"
+            save_result(ori_x, ori_y, y_pred, save_path)
+
+            """ Flattening the numpy arrays. """
+            y = y.flatten()
+            y_pred = y_pred.flatten()
+
+            """ Calculating metrics values """
+            acc_value = accuracy_score(y, y_pred)
+            f1_value = f1_score(y, y_pred, labels=[0, 1], average="binary")
+            jac_value = jaccard_score(y, y_pred, labels=[0, 1], average="binary")
+            recall_value = recall_score(y, y_pred, labels=[0, 1], average="binary")
+            precision_value = precision_score(y, y_pred, labels=[0, 1], average="binary")
+            SCORE.append([name, acc_value, f1_value, jac_value, recall_value, precision_value])
+
+        """ Metrics values """
+        score = [s[1:]for s in SCORE]
+        score = np.mean(score, axis=0)
+        print(f"Accuracy: {score[0]:0.5f}")
+        print(f"F1: {score[1]:0.5f}")
+        print(f"Jaccard: {score[2]:0.5f}")
+        print(f"Recall: {score[3]:0.5f}")
+        print(f"Precision: {score[4]:0.5f}")
+
+        """ Saving all the results """
+        df = pd.DataFrame(SCORE, columns=["Image", "Accuracy", "F1", "Jaccard", "Recall", "Precision"])
+        df.to_csv(cvc_path)
