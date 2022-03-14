8d41976efc843567e645086369df9a1882079d17
Luis <luis.abello@usach.cl>
2022-03-14 14:57:15 -0300

    dfsfsdf

--- model.py
+++ model.py
@@ -51,16 +51,16 @@ def build_unet(input_shape):
     """ Encoder """
     s1, p1 = encoder_block(inputs, 64)
     s2, p2 = encoder_block(p1, 128)
-    #s3, p3 = encoder_block(p2, 256)
+    s3, p3 = encoder_block(p2, 256)
     #s4, p4 = encoder_block(p3, 512)
 
     """ Bottleneck """
-    b1 = conv_block(p2, 1024)
+    b1 = conv_block(p3, 1024)
 
     """ Decoder """
     #d1 = decoder_block(b1, s4, 512)
-    #d2 = decoder_block(b1, s3, 256)
-    d3 = decoder_block(b1, s2, 128)
+    d2 = decoder_block(b1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
     d4 = decoder_block(d3, s1, 64)
 
     """ Output layer """
