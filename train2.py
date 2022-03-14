eb1f6c6579477dc7105a15bef52627bfc0df2667
Luis <luis.abello@usach.cl>
2022-03-14 03:23:48 -0300

    dasdasd

--- train2.py
+++ train2.py
@@ -639,7 +639,7 @@ if __name__ == "__main__":
     tf.set_random_seed(42)
 
     """ Directory to save files """
-    create_dir("files-menos-2-3-4-capa")
+    create_dir("files-menos-1-3-4-capa")
 
     """ Hyperparameters """
     batch_size = 2
@@ -647,8 +647,8 @@ if __name__ == "__main__":
     num_epochs = 150
 
     for i in range(1, 11):
-        model_path = "files-menos-2-3-4-capa/model"+str(i)+".h5"
-        csv_path = "files-menos-2-3-4-capa/data"+str(i)+".csv"
+        model_path = "files-menos-1-3-4-capa/model"+str(i)+".h5"
+        csv_path = "files-menos-1-3-4-capa/data"+str(i)+".csv"
 
         """ Dataset """
         dataset_path = "experimentos/imagenes/"
