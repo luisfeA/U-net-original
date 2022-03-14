f8a29e597eeb2dacdc0ee7b99ca5657a648ce813
Luis <luis.abello@usach.cl>
2022-03-13 23:47:30 -0300

    dadasdas

--- eval.py
+++ eval.py
@@ -106,7 +106,9 @@ if __name__ == "__main__":
     create_dir("experimentos/resultados/experimento9/caso3")
     create_dir("experimentos/resultados/experimento10/caso3")
 
-    for i in range(1, 11):
+    for i in range(2, 3):
+        save_path = ''
+        name = ''
         (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(dataset_path)
         """ Prediction and metrics values """
 
