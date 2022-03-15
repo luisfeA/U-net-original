3072f45aa9dafceeb0396f320ca647c0c4dca0d5
Luis <luis.abello@usach.cl>
2022-03-15 15:13:35 -0300

    dasdasd

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
+from tensorflow.keras.layers import Conv2D, BatchNormalization, \
+    Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
 from tensorflow.keras.models import Model
 import tensorflow as tf
 
@@ -34,41 +35,34 @@ def decoder_block(inputs, skip_features, num_filters):
 
     delta2 = skip_features_size2 - inputs_size2
     delta2 // 2
-
     dif2 = (skip_features_size2) - (inputs_size2-delta2)
-    print(delta2)
-    print(skip_features.shape)
-    print(x.shape)
-    skip_features = skip_features[:,delta:(skip_features_size+delta)-dif,:skip_features_size2-delta2,:]
+    skip_features = skip_features[:, delta:(skip_features_size+delta)-dif, :skip_features_size2-delta2, :]
     x = Concatenate()([x, skip_features])
     x = conv_block(x, num_filters)
     return x
 
 def build_unet(input_shape):
-    """ Input layer """
+    """ Imagen de entrada """
     inputs = Input(input_shape)
 
-    """ Encoder """
+    """ Codificador """
     s1, p1 = encoder_block(inputs, 64)
-    #s2, p2 = encoder_block(p1, 128)
-    #s3, p3 = encoder_block(p2, 256)
-    #s4, p4 = encoder_block(p3, 512)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
 
-    """ Bottleneck """
-    b1 = conv_block(p1, 1024)
+    """ Capa intermedia """
+    b1 = conv_block(p4, 1024)
 
-    """ Decoder """
-    #d1 = decoder_block(b1, s4, 512)
-    #d2 = decoder_block(b1, s3, 256)
-    #d3 = decoder_block(b1, s2, 128)
-    d4 = decoder_block(b1, s1, 64)
+    """ Decodificador """
+    d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(d1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
+    d4 = decoder_block(d3, s1, 64)
 
-    """ Output layer """
+    """ Resultado """
     outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
-    #rank_4_tensor = tf.zeros([768,576,1])
-    #x = tf.add(outputs, rank_4_tensor)
     outputs = tf.image.resize(outputs, [580, 780])
-    #print(x.shape)
     model = Model(inputs, outputs, name="UNET")
     return model
 
