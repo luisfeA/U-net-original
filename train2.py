9b66c5c1d6071c989f6897f197df66144b68bfe3
Luis <luis.abello@usach.cl>
2022-05-08 02:10:06 -0400

    cxzczx

--- train2.py
+++ train2.py
@@ -647,7 +647,7 @@ if __name__ == "__main__":
     lr = 1e-4   ## 0.0001
     num_epochs = 150
 
-    for i in range(13, 15):
+    for i in range(15, 17):
         model_path = "files-hyperparameters/model"+str(i)+".h5"
         csv_path = "files-hyperparameters/data"+str(i)+".csv"
 
