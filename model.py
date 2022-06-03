9eab6441b3ff12454f62e71b1aa6354756755e0e
Luis <luis.abello@usach.cl>
2022-06-03 10:10:32 -0400

    asdasdsad

--- model.py
+++ model.py
@@ -47,16 +47,16 @@ def build_unet(input_shape):
     """ Codificador """
     s1, p1 = encoder_block(inputs, 64)
     s2, p2 = encoder_block(p1, 128)
-    #s3, p3 = encoder_block(p1, 256)
-    s4, p4 = encoder_block(p2, 512)
+    s3, p3 = encoder_block(p2, 256)
+    #s4, p4 = encoder_block(p2, 512)
 
     """ Capa intermedia """
-    b1 = conv_block(p4, 1024)
+    b1 = conv_block(p3, 1024)
 
     """ Decodificador """
-    d1 = decoder_block(b1, s4, 512)
-    #d2 = decoder_block(d1, s3, 256)
-    d3 = decoder_block(d1, s2, 128)
+    #d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(b1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
     d4 = decoder_block(d3, s1, 64)
 
     """ Resultado """
