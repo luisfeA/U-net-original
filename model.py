ed546d2b586ddac532fe739c5ddb3c9e61a3467d
Luis <luis.abello@usach.cl>
2022-06-03 11:48:02 -0400

    dsadasds

--- model.py
+++ model.py
@@ -47,17 +47,17 @@ def build_unet(input_shape):
     """ Codificador """
     s1, p1 = encoder_block(inputs, 64)
     #s2, p2 = encoder_block(inputs, 128)
-    #s3, p3 = encoder_block(p2, 256)
-    s4, p4 = encoder_block(p1, 512)
+    s3, p3 = encoder_block(p1, 256)
+    #s4, p4 = encoder_block(p1, 512)
 
     """ Capa intermedia """
-    b1 = conv_block(p4, 1024)
+    b1 = conv_block(p3, 1024)
 
     """ Decodificador """
-    d1 = decoder_block(b1, s4, 512)
-    #d2 = decoder_block(b1, s3, 256)
+    #d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(b1, s3, 256)
     #d3 = decoder_block(d2, s2, 128)
-    d4 = decoder_block(d1, s1, 64)
+    d4 = decoder_block(d2, s1, 64)
 
     """ Resultado """
     outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
